110923994632a099280b95e0f92c705df5bc16f1
Guilherme Chiarotto <guichiarotto10@gmail.com>
2024-06-10 20:50:02 -0300

    Telas

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,8 @@ import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import Login from "@/components/navigation/Login";
 import Register from "@/components/navigation/Register";
+import TreinosCadastrados from "@/components/navigation/TreinosCadastrados";
+import Perfil from "@/components/navigation/Perfil";
 
 
 export default function Index() {
